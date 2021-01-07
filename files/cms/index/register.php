7d5d1ae1a41089a2b755523a6937faef244c7589
Lucas Barbosa <lucashp03@gmail.com>
2021-01-07 14:23:05 -0300

    Update register.php

--- files/cms/index/register.php
+++ files/cms/index/register.php
@@ -22,57 +22,57 @@
 								<icon name="search"></icon>
 							</div>
 							<label class="white flex-column">
-								<h4 class="bold">Registre-se agora</h4>
-								<h5>Junte-se a nós hoje!</h5>
+								<h4 class="bold">Register now</h4>
+								<h5>Join us today!</h5>
 							</label>
 						</div>
 						<form method="POST" class="padding-minm padding-top-none register-form">
 							<div class="flex-column margin-bottom-min">
 								<label class="gray margin-bottom-minm">
-									<h6>Escolha seu nome de usuário sabiamente, <b>não toleramos vulgaridades em nomes de usuários!</b><br><br>E o seu nome, também, deve ter entre <b>4 e 15 letras e sem caracteres especiais</b>.</h6>
+									<h6>Choose your username wisely, <b>we do not tolerate vulgarities in usernames!</b><br><br>And your name, too, must be between <b>4 and 15 letters and no special characters</b>.</h6>
 								</label>
 								<div class="register-inputs width-content flex">
 									<icon name="head-mini"></icon>
-									<input type="text" name="username" placeholder="Nome de usuário(a)" data-input="username" id="username">
+									<input type="text" name="username" placeholder="You username" data-input="username" id="username">
 									<div class="reg-status username pointer-none"></div>
 								</div>										
 							</div>
 							<div class="flex-column margin-bottom-min">
 								<label class="gray margin-bottom-minm">
-									<h6>Certifique-se de utilizar um e-mail <b>válido</b> e <b>verdadeiro</b>, pois caso necessário para recuperação de senhas, entrar em contato e dentre outro, entraremos em contato pelo mesmo.</h6>
+									<h6>Be sure to use an email <b>valid</b> and <b>true</b> because if necessary for password recovery, contact and among others, we will contact you.</h6>
 								</label>
 								<div class="register-inputs width-content flex">
 									<icon name="email"></icon>
-									<input type="text" name="email" placeholder="Endereço de e-mail" data-input="email" id="email">
+									<input type="text" name="email" placeholder="Your e-mail" data-input="email" id="email">
 									<div class="reg-status email pointer-none"></div>
 								</div>
 							</div>
 							<div class="flex-column margin-bottom-min">
 								<label class="gray margin-bottom-minm">
-									<h6>Segurança nunca é demais! Utilize <b>uma senha segura</b> e fácil de você lembrar, outra opção é <b>aceitar sugestões de senhas pelo seu próprio navegador</b>, a senha fica salva nele ao fazer login, facilitando mais e te deixando seguro(a).</h6>
+									<h6>Security is never too much! Use <b>a secure password</b> and easy to remember, another option is to <b>accept password suggestions by your own browser</b>, the password is saved on it when logging in, making it easier and making you safe.</h6>
 								</label>
 								<div class="register-inputs width-content flex">
 									<icon name="lock-2"></icon>
-									<input type="password" name="password" placeholder="Senha" data-input="password">
+									<input type="password" name="password" placeholder="Your Password" data-input="password">
 									<div class="reg-status password pointer-none"></div>
 								</div>
 							</div>
 							<div class="flex-column margin-bottom-min">
 								<label class="gray margin-bottom-minm">
-									<h6>Sua data de nascimento. <b>(Recomeandado para maiores de 13 anos)</b></h6>
+									<h6>Your date of birth. <b> (Restarted for over 13 years)</b>></h6>
 								</label>
 								<div class="register-inputs">
 									<div class="flex reg-birthday">
 										<icon name="head" class="margin-right-md margin-auto-top-bottom margin-left-min"></icon>
 										<div class="flex width-content">
 											<select name="birthday" class="days margin-right-min">
-												<option value="DD" selected disabled>Dia</option>
+												<option value="DD" selected disabled>Day</option>
 											</select>
 											<select name="birthmonth" class="months margin-right-min">
-												<option value="MM" selected disabled>Mês</option>
+												<option value="MM" selected disabled>Month</option>
 											</select>
 											<select name="birthyear" class="years">
-												<option value="YYYY" selected disabled>Ano</option>
+												<option value="YYYY" selected disabled>Year</option>
 											</select>
 										</div>
 									</div>
@@ -80,7 +80,7 @@
 								</div>
 							</div>
 							<label class="gray">
-								<h6>Além de ser <b>obrigatório</b>, a <b>escolha de sexo</b> é essêncial para que ao você se registrar você possa receber o seu cafofo bem decorado e um presente bem legal, além de também identificar o seu gênero de acordo com a sua escolha.</h6>
+								<h6>In addition to being <b>mandatory</b>, the <b>gender choice</b> is essential so that when you register you can receive your well decorated cafofo and a nice gift, in addition to also identifying your gender according to your choice.</h6>
 							</label>
 							<div class="flex margin-top-min">
 								<div class="flex width-content">
@@ -90,7 +90,7 @@
 											<div class="gender-female-option pointer-none">
 												<div class="flex">
 													<icon name="female"></icon>
-													<h6 class="margin-auto-top-bottom">Sexo feminino</h6>
+													<h6 class="margin-auto-top-bottom">Gender Woman</h6>
 												</div>
 											</div>
 										</label>
@@ -101,7 +101,7 @@
 											<div class="gender-male-option pointer-none">
 												<div class="flex">
 													<icon name="male"></icon>
-													<h6 class="margin-auto-top-bottom">Sexo masculino</h6>
+													<h6 class="margin-auto-top-bottom">Gender Male</h6>
 												</div>
 											</div>
 										</label>
@@ -136,15 +136,15 @@
 					<label class="gray flex-column">
 						<h2 class="bold margin-bottom-min">Venha conhecer o <?= HOTELNAME; ?>!</h2>
 						<div class="flex margin-auto-bottom">
-							<h5 class="fs-14">O <b><?= HOTELNAME; ?></b> é um comunidade virtua de pixels onde você pode criar seu próprio avatar, fazer muitos amigos, bater-papo com diversos usuários e usuárias do nosso hotel, construir e decorar seus próprios quartos, criar seus próprios jogos ou jogar os de outros usuários e muitos mais...</h5>
+							<h5 class="fs-14">O <b><?= HOTELNAME; ?></b> is a virtua community of pixels where you can create your own avatar, make many friends, chat with various users and users of our hotel, build and decorate your own rooms, create your own games or play those of other users and many more...</h5>
 							<img class="margin-auto-top-bottom" style="min-width:200px" height="123px" src="https://images.habbo.com/c_images/WhatIsHabbo/ill_17.png">
 						</div>
 						<div class="flex margin-top-md">
 							<img class="margin-auto-top-bottom" style="min-width:200px" height="171px" src="https://images.habbo.com/c_images/WhatIsHabbo/ill_16.png">
-							<h5 class="text-right fs-14">Criatividade e originalidade são super bem-vindas no <b><?= HOTELNAME; ?></b>! Toda semana temos várias competições novas para você participar. De competições de quarto, atividades legais onde você pode expressar todos os seus dons artísticos e criativos e, ainda por cima, ganhar conquistas e prêmios! Bateu a inspiração? Dê uma olhada nas nossas <b>notícias</b> para ficar por dentro das últimas atividades e competições da semana!</h5>
+							<h5 class="text-right fs-14">Creativity and originality are super welcome in <b><?= HOTELNAME; ?></b>! Every week we have several new competitions for you to participate in. From bedroom competitions, cool activities where you can express all your artistic and creative gifts and, on top of that, win achievements and prizes! Did you hit the inspiration? Take a look at our <b> news</b> to stay on top of the latest activities and competitions of the week!</h5>
 						</div>
 						<div class="flex margin-auto-top">
-							<h5 class="fs-14">Você adora bater papo e encontrar os seus amigos? os <b>Haibbo Grupos, Fórums e comunidades de RPG</b> são ótimas opções para você. Entre no exército e assuma seus deveres, monte a sua própria escola e decida você mesmo o que estudar, e arrase na passarela ou corra para a emergência e comece a salvar vidas pixeladas.</h5>
+							<h5 class="fs-14">Do you love chatting and meeting your friends? The <b>Haibbo Groups, Forums and RPG communities</b> are great options for you. Enter the army and take on your duties, set up your own school and decide for yourself what to study, and rock the catwalk or run to the emergency room and start saving pixelated lives.</h5>
 							<img class="margin-auto-top-bottom" style="min-width:198px" height="157px" src="<?= CDN; ?>/assets/img/custom/comments.png">
 						</div>
 					</label>
